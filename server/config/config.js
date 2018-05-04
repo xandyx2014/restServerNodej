1e22a6f809056f093803463dcb3538b95bdc064e
xandyx2014 <xandyx2014@gmail.com>
2018-05-04 12:48:55 -0400

    Variable de entorno MOngo

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB
 if(process.env.NODE_ENV ==='dev'){
     urlDB='mongodb://localhost:27017/cafe'
 }else{
-    urlDB='mongodb://xandyx2014:UPDATE33todeath@ds115340.mlab.com:15340/cafe'
+    urlDB=process.env.MONGO_URI
 }
 process.env.URLDB=urlDB
 
